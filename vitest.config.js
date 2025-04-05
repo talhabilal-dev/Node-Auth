759b8adb1154c17db13577b89254d7d8e3241ee5
talhabilal-dev <m.talha.bilal.dev@gmail.com>
2025-04-05 22:54:48 +0500

    fix: update test timeout in Vitest configuration from 10 seconds to 20 seconds

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     exclude: ["**/node_modules/**"], // Exclude node_modules from tests
-    testTimeout: 20000, // Set a timeout for tests (10 seconds)
+    testTimeout: 20000, // Set a timeout for tests (20 seconds)
     globals: true, // Ensure Vitest globals are enabled
     environment: "node", // Make sure you're using a Node.js environment for tests
   },
